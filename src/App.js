5bb186c8cbc96f6ff740880a40a7bda69ac631db
Jose Sanchez <sanchezjjose@gmail.com>
2017-10-09 22:10:41 -0400

    adding handleSubmit method

--- src/App.js
+++ src/App.js
@@ -3,15 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(e.target.className);
+    console.log(e.target.href);
+    console.log('button clicked');
+
+    //fetch('http://sanchezjjose:8080/on');
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React@</h1>
+          <h1 className="App-title">Welcome to Home Assistant</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <a href='/on' className='turn-on' onClick={this.handleSubmit}>Turn On</a>
+          <a href='/off' className='turn-off' onClick={this.handleSubmit}>Turn Off</a>
         </p>
       </div>
     );

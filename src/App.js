86295ebc22a0dbc4adf12de20de132e8cf473019
josanche <jose.sanchez@mlb.com>
2017-10-11 08:57:21 -0400

    add button clicked handler

--- src/App.js
+++ src/App.js
@@ -4,17 +4,47 @@ import './App.css';
 
 class App extends Component {
 
-  handleSubmit(e) {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOn: false 
+    };
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
     e.preventDefault();
 
     console.log(e.target.className);
     console.log(e.target.href);
     console.log('button clicked');
 
-    //fetch('http://sanchezjjose:8080/on');
+    console.log(this.props);
+
+    this.setState(prevState => ({
+      isOn: !prevState.isOn
+    }));
+
+    console.log(this.state.isOn);
+
+    //fetch(this.props.SERVER_URL)
+      //.then(response => {
+        //response.json().then(data => {
+          //console.log('Data', data);
+        //});
+      //})
+      //.catch(err => {
+        //console.log('Error: ', err.message);
+      //})
   }
 
   render() {
+
+    console.log(this.props);
+
     return (
       <div className="App">
         <header className="App-header">
@@ -22,8 +52,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to Home Assistant</h1>
         </header>
         <p className="App-intro">
-          <a href='/on' className='turn-on' onClick={this.handleSubmit}>Turn On</a>
-          <a href='/off' className='turn-off' onClick={this.handleSubmit}>Turn Off</a>
+          <a href='/on' className='button turn-on' onClick={this.handleClick}>Turn On</a>
+          <a href='/off' className='button turn-off' onClick={this.handleClick}>Turn Off</a>
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
+ReactDOM.render(<App url={SERVER_URL} />, document.getElementById('root'));
 registerServiceWorker();

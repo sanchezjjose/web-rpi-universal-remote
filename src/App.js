e5db85fc532bb4f821d4e78504f98b2bf886c5fc
josanche <jose.sanchez@mlb.com>
2017-10-13 16:20:53 -0400

    create main card component

--- src/App.js
+++ src/App.js
@@ -8,29 +8,57 @@ class App extends Component {
     super(props);
 
     this.state = {
-      isOn: false 
+      isOff: true,
+      isOn: false,
+      mode: 'dry',
+      speed: 'auto',
+      temp: '72'
     };
 
     // This binding is necessary to make `this` work in the callback
     this.handleClick = this.handleClick.bind(this);
+    this.buildRequestUrl = this.buildRequestUrl.bind(this);
   }
 
-  handleClick(e) {
+  componentDidMount() {
+    fetch(this.props.url + '/status')
+      .then(response => {
+        response.json().then(data => {
+          this.setState({
+            isOn: data.isOn,
+            isOff: data.isOff,
+            mode: data.settings.mode || this.state.mode,
+            speed: data.settings.speed || this.state.speed,
+            temp: data.settings.temp ||  this.state.temp
+          });
+        });
+      })
+      .catch(err => {
+        console.log('Error: ', err.message);
+      })
+  }
+
+  buildRequestUrl() {
+    const endpoint = this.state.isOff ? 'on' : 'set'; 
+    const query = `mode=${this.state.mode}&speed=${this.state.speed}&temp=${this.state.temp}`
+
+    return `${this.props.url}/${endpoint}?${query}`
+  }
+
+  handleClick(turnOn, mode, e) {
     e.preventDefault();
 
-    console.log(e.target.className);
-    console.log(e.target.href);
-    console.log('button clicked');
-
-    console.log(this.props);
+    const requestUrl = this.buildRequestUrl();
 
     this.setState(prevState => ({
-      isOn: !prevState.isOn
+      isOn: turnOn,
+      isOff: !turnOn,
+      mode: mode
     }));
 
-    console.log(this.state.isOn);
+    console.log(requestUrl);
 
-    //fetch(this.props.SERVER_URL)
+    //fetch(this.props.url)
       //.then(response => {
         //response.json().then(data => {
           //console.log('Data', data);
@@ -42,16 +70,22 @@ class App extends Component {
   }
 
   render() {
-
-    console.log(this.props);
-
     return (
       <div className="App">
-        <Header />
-        <p className="App-intro">
-          <a href='/on' className='button turn-on' onClick={this.handleClick}>Turn On</a>
-          <a href='/off' className='button turn-off' onClick={this.handleClick}>Turn Off</a>
-        </p>
+          <Header />
+          <main className="main">
+              <div className="card air-conditioner">
+                  <div className='temperature'>
+                    {this.state.isOn ? 'On ' + this.state.mode + this.state.speed + this.state.temp : 'Off'}
+                  </div>
+
+                  <button className='button' onClick={this.handleClick.bind(this, true, 'cool')}>Turn On COOL</button>
+                  <button className='button' onClick={this.handleClick.bind(this, true, 'heat')}>Turn On HEAT</button>
+                  <button className='button' onClick={this.handleClick.bind(this, true, 'dry')}>Turn On DRY</button>
+                  <button className='button' onClick={this.handleClick.bind(this, true, 'fan')}>Turn On FAN</button>
+                  <button className='button' onClick={this.handleClick.bind(this, false, '')}>Turn Off</button>
+              </div>
+          </main>
       </div>
     );
   }
